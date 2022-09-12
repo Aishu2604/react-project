262775866139c7a5aecc8d5cac84ec582c07af28
Aishu2604 <aishwary.nigam04@gmail.com>
2022-09-13 00:17:38 +0530

    Lets Start Coding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>
+        Hello! Mr. Aishwary
+      </h2>
+      <ExpenseItem>
+      </ExpenseItem>
+      <p>
+      Food Rs 10
+      <br></br>
+      Petrol Rs 100
+      <br></br>
+      Movies Rs 200
+      </p>
     </div>
   );
 }
