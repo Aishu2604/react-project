7a6a5d7a119f4cc57d62f73b03bd14074c489be7
Aishu2604 <aishwary.nigam04@gmail.com>
2022-09-19 00:01:34 +0530

    Submitting the form

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -17,21 +17,35 @@ const ExpenseForm = () => {
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
     }
+    
+    const submitHandler = (event) =>{
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
 
     return (
-     <form>
+     <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__controls">
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler}/>
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
             </div>
             <div className="new-expense__controls">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
             </div>
             <div className="new-expense__controls">
                 <label>Date</label>
-                <input type="date" min="2022-01-01" max="2023-12-31" onChange={dateChangeHandler} />
+                <input type="date" value={enteredDate} min="2022-01-01" max="2023-12-31" onChange={dateChangeHandler} />
             </div>
         </div>
         <div className="new-expense__actions">
