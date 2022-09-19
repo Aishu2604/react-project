8c9f44c10961d029d839e59f5d263d73511330a3
Aishu2604 <aishwary.nigam04@gmail.com>
2022-09-20 03:53:35 +0530

    Advance React Completed

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import './Expense.css';
 import ExpenseFilter from "./ExpenseFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2022');
@@ -19,6 +20,7 @@ const Expenses = (props) => {
     return (
         <Card className="expenses">
           <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangedHandler} />
+          <ExpensesChart expenses={filteredExpenses} />
           <ExpensesList item={filteredExpenses} />
         </Card>
       );
--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -8,9 +8,6 @@ const ExpensesList = (props) => {
   if (props.item.length === 0) {
     return <h2 className="expenses-list__fallback"> Found No Expenses!!! </h2>
   }
-  if (props.item.length === 1) {
-    return <h2 className="expenses-list__fallback"> Add More Expenses!!! </h2>
-  }
   return (
     <ul className="expenses-list">
         {props.item.map((expense) =>(
